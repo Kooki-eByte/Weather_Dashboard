9ac77bdb864b4f5ed96a8dc1cc30b7feb9c4ad48
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-22 23:17:20 -0400

    Started on javascript, have starting variables and have the date grabbed it is just now figuring out how to make it so the ajax can be used only when the user presses a button

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,74 @@
-//  http://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={your api key}
+var lat;
+var lon;
+// let cityName = "boston";
+
+const listHolder = $("#search-holder");
+
+const now = moment().format("LL");
+console.log(now);
+
+function addCityToList(city) {
+  // <li class="list-group-item" id="city-0">Boston</li>
+  let createLi = $("<li>");
+  createLi.text(city);
+  createLi.attr("class", "list-group-item");
+  createLi.attr("id", "city-0");
+  listHolder.prepend(createLi);
+}
+
+function displayMainForecast(queryUrl) {}
+
+function display5DayForecast() {}
+
+// $.ajax({
+//   url: queryUrlMain,
+//   method: "GET",
+// }).then(function (response) {
+//   console.log("------- One day forecast -------");
+//   lat = response.coord.lat;
+//   lon = response.coord.lon;
+//   console.log(response);
+//   console.log(response.name); // city name
+//   console.log(response.main.temp); // temp in kelvin
+//   console.log(response.main.humidity); // humidity
+//   console.log(response.wind.speed); // wind speed
+//   //   console.log(response);
+//   let queryUrl5Day =
+//     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+//     lat +
+//     "&lon=" +
+//     lon +
+//     "&exclude=current,minutely,hourly&appid=b69a42c83210378fa102751081b2696f";
+
+//   $.ajax({
+//     url: queryUrl5Day,
+//     method: "GET",
+//   }).then(function (response) {
+//     console.log("------- 5 day forecast -------");
+//     console.log(response);
+//   });
+// });
+
+$(".search-btn").on("click", function (event) {
+  event.preventDefault();
+
+  let cityName = $(".user-search").val();
+  addCityToList(cityName);
+
+  let queryUrlMain =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=b69a42c83210378fa102751081b2696f";
+  displayMainForecast(queryUrlMain);
+
+  if (cityName !== "") {
+    $.ajax({
+      url: queryUrlMain,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
+  } else {
+    return $(".user-search").val("");
+  }
+});
